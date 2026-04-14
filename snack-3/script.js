8e8d7a31a374026226a82f45651c2ae04c898baa
“Nilde <nilde.benedetti@gmail.com>
2026-04-14 15:56:20 +0200

    snack 3 - soluzione con map

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numbersIncreased = numbers.map( function (valore) {
+    return valore +1
+});
+
+console.log(numbersIncreased);
